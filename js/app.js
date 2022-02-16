be15b4c4ee02724da52c6f6d55524fe0a0a26c8d
mamuntufayel <mamuntuffa@gmail.com>
2022-02-16 17:32:35 -0500

    calculate done

--- js/app.js
+++ js/app.js
@@ -1,21 +1,21 @@
 
 function calculateTotal() {
-    // console.log('clicked');
     const foodExpances = document.getElementById('food-expenses').value;
     const rentExoances = document.getElementById('rent-expenses').value;
     const clothingExoances = document.getElementById('clothes-expenses').value;
     const totalExpancesInSum = parseInt(foodExpances) + parseInt(rentExoances) + parseInt(clothingExoances);
     console.log(totalExpancesInSum);
     const totalExpance = document.getElementById('total-expances');
-    const totalExpanceValue = totalExpance.innerText;
     totalExpance.innerText = totalExpancesInSum;
-    // return totalExpance.innerText;
+
+
+    const totalIncomeInput = document.getElementById('total-income');
+    const totalIncomeInputValue = totalIncomeInput.value;
+    const balanceInput = document.getElementById('remaining-balance');
+    const balanceInputValue = balanceInput.innerText;
+    const gettingBalance = parseInt(totalIncomeInputValue) - parseInt(totalExpancesInSum);
+    balanceInput.innerText = gettingBalance;
 }
 
-// const totalIncome = document.getElementById('total-income');
-// const totalIncomeValue = totalIncome.value;
-// const remainingBalanceInput = document.getElementById('the-balance');
-// const remainingBalanceInputValue = remainingBalanceInput.innerText;
-// const remainingBalanceValue = totalIncomeValue - calculateTotal();
-// remainingBalanceInput.innerText = remainingBalanceValue;
+
 
