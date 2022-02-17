bf6934054ec0cecfb98e44c850d03ab357d7c933
mamuntufayel <mamuntuffa@gmail.com>
2022-02-16 20:30:50 -0500

    commited of all calculation

--- js/app.js
+++ js/app.js
@@ -1,11 +1,14 @@
-
 function calculateTotal() {
     const foodExpances = document.getElementById('food-expenses').value;
-    const rentExoances = document.getElementById('rent-expenses').value;
+
+    const rentExpances = document.getElementById('rent-expenses').value;
+
     const clothingExoances = document.getElementById('clothes-expenses').value;
-    const totalExpancesInSum = parseInt(foodExpances) + parseInt(rentExoances) + parseInt(clothingExoances);
+
+    const totalExpancesInSum = parseInt(foodExpances) + parseInt(rentExpances) + parseInt(clothingExoances);
     console.log(totalExpancesInSum);
     const totalExpance = document.getElementById('total-expances');
+
     totalExpance.innerText = totalExpancesInSum;
 
 
@@ -15,6 +18,27 @@ function calculateTotal() {
     const balanceInputValue = balanceInput.innerText;
     const gettingBalance = parseInt(totalIncomeInputValue) - parseInt(totalExpancesInSum);
     balanceInput.innerText = gettingBalance;
+}
+
+function getSaving() {
+    const totalIncomeInput = document.getElementById('total-income');
+    const totalIncomeInputValue = parseInt(totalIncomeInput.value);
+    const inputBox = document.getElementById('input-parcentage');
+    const inputboxvalue = parseInt(inputBox.value);
+    // console.log(inputboxvalue);
+    const savingAmountText = document.getElementById('saving-amount');
+    const savingAmountInValue = savingAmountText.innerText;
+    const theSaving = (totalIncomeInputValue * inputboxvalue) / 100;
+    savingAmountText.innerText = theSaving;
+    // gettingBalance -------------------------
+    const previousBalance = document.getElementById('remaining-balance');
+    const previousBalanceValue = parseInt(previousBalance.innerText);
+    const balanceAfterSaving = document.getElementById('balance-after-saving');
+    const balanceAterSavingValue = parseInt(balanceAfterSaving.innerText);
+    const theRemainingBalance = previousBalanceValue - theSaving;
+    balanceAfterSaving.innerText = theRemainingBalance;
+
+
 }
 
 
