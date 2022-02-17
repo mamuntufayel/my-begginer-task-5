28322b14369b74eed455f3b701aef51fb6fd86dd
mamuntufayel <mamuntuffa@gmail.com>
2022-02-17 01:09:58 -0500

    comment added

--- js/app.js
+++ js/app.js
@@ -3,16 +3,27 @@ function calculateTotal() {
     const foodExpances = document.getElementById('food-expenses').value;
     const rentExpances = document.getElementById('rent-expenses').value;
     const clothingExoances = document.getElementById('clothes-expenses').value;
+    // Getting total expances ----------------
     const totalExpancesInSum = parseInt(foodExpances) + parseInt(rentExpances) + parseInt(clothingExoances);
-    console.log(totalExpancesInSum);
     const totalExpance = document.getElementById('total-expances');
     totalExpance.innerText = totalExpancesInSum;
     const totalIncomeInput = document.getElementById('total-income');
     const totalIncomeInputValue = totalIncomeInput.value;
+    // Condition for minus Value ---------
+    if (totalExpancesInSum > totalIncomeInputValue) {
+        document.getElementById('total-income').style.backgroundColor = 'red';
+        document.getElementById('remaining-balance').style.backgroundColor = 'red';
+
+    }
     const balanceInput = document.getElementById('remaining-balance');
     const balanceInputValue = balanceInput.innerText;
     const gettingBalance = parseInt(totalIncomeInputValue) - parseInt(totalExpancesInSum);
     balanceInput.innerText = gettingBalance;
+    // reset inputfield ----------------
+    // totalIncomeInput.value = '';
+    document.getElementById('food-expenses').value = '';
+    document.getElementById('rent-expenses').value = '';
+    document.getElementById('clothes-expenses').value = '';
 }
 
 function getSavingMoney() {
@@ -29,8 +40,11 @@ function getSavingMoney() {
     const previousBalanceValue = parseInt(previousBalance.innerText);
     const balanceAfterSaving = document.getElementById('balance-after-saving');
     const balanceAterSavingValue = parseInt(balanceAfterSaving.innerText);
+    // Getting remaining balance -----------------
     const theRemainingBalance = previousBalanceValue - theSaving;
     balanceAfterSaving.innerText = theRemainingBalance;
+    // reset input field -----------------
+    inputBox.value = '';
 }
 
 
